42c7e43b8ff217f338d76f56a39d51df70977a8a
Daniel Ha <daniel.ha@spin.pm>
2021-10-26 10:48:22 -0700

    Refactor indentation

--- src/components/movie-list/index.js
+++ src/components/movie-list/index.js
@@ -5,7 +5,7 @@ function Movie({movie}) {
     const { imdbID, Title } = movie;
     return (
         <li key={imdbID} className="slide-up-fade-in py-10">
-            <span>{Title} </span>
+            <span>{Title}</span>
         </li>
     )
 }
@@ -43,7 +43,7 @@ function MovieList() {
       <ul className="mt-50 styled" data-testid="movieList">
         {movies != null && movies.map(movie => <Movie key={movie.imdbID} movie={movie} />)}
       </ul>
-        {movies != null && !movies.length && <div className="mt-50 slide-up-fade-in" data-testid="no-result">No Results Found</div>}
+      {movies != null && !movies.length && <div className="mt-50 slide-up-fade-in" data-testid="no-result">No Results Found</div>}
     </div>
   );
 }
